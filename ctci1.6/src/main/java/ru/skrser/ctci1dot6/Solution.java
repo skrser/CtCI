234d55b114c6707e0dcc7e70f9c9c835a353e290
Sergey Skryabin <sskryabin@diasoft.ru>
2017-01-01 18:23:55 +0300

    question 1.6

--- ctci1.6/src/main/java/ru/skrser/ctci1dot6/Solution.java
+++ ctci1.6/src/main/java/ru/skrser/ctci1dot6/Solution.java
@@ -5,4 +5,36 @@ package ru.skrser.ctci1dot6;
  * on 01.01.2017.
  */
 public class Solution {
+
+    public static void rotate(byte[][][] image) {
+        if (image == null)
+            return;
+        if (image.length != image[0].length) // TODO
+            throw new IllegalArgumentException("Image should be square to rotate!");
+        int quater = image.length/2 + image.length%2;
+        for (int i = 0; i < quater;i++) {
+            for (int j = 0; j < quater; j++) {
+                rotatePixel(image, i, j);
+            }
+        }
+    }
+
+    private static void rotatePixel(byte[][][] image, int x, int y) {
+        byte[] tmpPixel = image[x][y];
+
+        int prevX = x;
+        int prevY = y;
+        int nextX;
+        int nextY;
+
+        for (int i = 0; i< 3; i++) {
+            nextX = image.length - 1 - prevY;
+            nextY = image.length - 1 - prevX;
+            image[prevX][prevY] = image[nextX][nextY];
+            prevX = nextX;
+            prevY = nextY;
+        }
+        image[prevX][prevY] = tmpPixel;
+    }
+
 }
