23e70d208ffb629fb604ae1582d58722837b74eb
Sergey Skryabin <sskryabin@diasoft.ru>
2017-01-01 17:00:15 +0300

    fixing type ctcl -> ctci question 1.5

--- ctci1.5/src/main/java/ru/skrser/ctci1dot5/Solution.java
+++ ctci1.5/src/main/java/ru/skrser/ctci1dot5/Solution.java
@@ -5,4 +5,32 @@ package ru.skrser.ctci1dot5;
  * on 01.01.2017.
  */
 public class Solution {
+    /**
+     * Compresses string, e.g. aaaabcc -> a4b1c2
+     * Computational complexity = O(n)
+     * Memory consumption = O(1)
+     *
+     * @param str string to compress
+     * @return compressed string or original, if compressed is not smaller
+     */
+    public static String compress(String str) {
+        if (str == null || str.length() <= 2)
+            return str;
+        StringBuilder sb = new StringBuilder();
+        char bufferChar = str.charAt(0);
+        int bufferCount = 1;
+        for (int i = 1; i < str.length(); i++) {
+            if (str.charAt(i) == bufferChar)
+                bufferCount++;
+            else {
+                sb.append(bufferChar).append(bufferCount);
+                bufferChar = str.charAt(i);
+                bufferCount = 1;
+            }
+        }
+        sb.append(bufferChar).append(bufferCount);
+        String compressedString = sb.toString();
+        return str.length() <= compressedString.length() ? str : compressedString;
+    }
+
 }
--- ctci1.5/src/test/java/ru/skrser/ctci1dot5/test/SolutionTest.java
+++ ctci1.5/src/test/java/ru/skrser/ctci1dot5/test/SolutionTest.java
@@ -1,8 +1,25 @@
 package ru.skrser.ctci1dot5.test;
 
+import org.junit.Test;
+import ru.skrser.ctci1dot5.Solution;
+
+import static org.junit.Assert.assertEquals;
+
 /**
  * Created by Sergey Skryabin
  * on 01.01.2017.
  */
 public class SolutionTest {
+
+    @Test
+    public void testPosisive() {
+        assertEquals("a4b1c2", Solution.compress("aaaabcc"));
+        assertEquals("aaabcc", Solution.compress("aaabcc"));
+        assertEquals(null, Solution.compress(null));
+        assertEquals("a", Solution.compress("a"));
+        assertEquals("aa", Solution.compress("aa"));
+        assertEquals("aab", Solution.compress("aab"));
+        assertEquals("a3", Solution.compress("aaa"));
+        assertEquals("a4b1c2a4b1c2", Solution.compress("aaaabccaaaabcc"));
+    }
 }
