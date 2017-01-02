eb97adfa64c2fb66e86b1f05b0fd5fb568e1220f
Sergey Skryabin <sskryabin@diasoft.ru>
2017-01-02 14:14:07 +0300

    question 1.6

--- ctci1.6/src/main/java/ru/skrser/ctci1dot6/Solution.java
+++ ctci1.6/src/main/java/ru/skrser/ctci1dot6/Solution.java
@@ -9,14 +9,21 @@ public class Solution {
     public static void rotate(byte[][][] image) {
         if (image == null)
             return;
-        if (image.length != image[0].length) // TODO
-            throw new IllegalArgumentException("Image should be square to rotate!");
-        int quater = image.length/2 + image.length%2;
-        for (int i = 0; i < quater;i++) {
-            for (int j = 0; j < quater; j++) {
+        checkImageIsSquare(image);
+
+        int quarterPlus = image.length / 2 + image.length % 2;
+        int quarterMinus = image.length / 2;
+        for (int i = 0; i < quarterPlus; i++) {
+            for (int j = 0; j < quarterMinus; j++) {
                 rotatePixel(image, i, j);
             }
         }
+    }
+
+    private static void checkImageIsSquare(byte[][][] image) {
+        for (byte[][] anImage : image)
+            if (anImage.length != image.length)
+                throw new IllegalArgumentException("Image should be square to rotate!");
     }
 
     private static void rotatePixel(byte[][][] image, int x, int y) {
@@ -27,9 +34,9 @@ public class Solution {
         int nextX;
         int nextY;
 
-        for (int i = 0; i< 3; i++) {
+        for (int i = 0; i < 3; i++) {
             nextX = image.length - 1 - prevY;
-            nextY = image.length - 1 - prevX;
+            nextY = prevX;
             image[prevX][prevY] = image[nextX][nextY];
             prevX = nextX;
             prevY = nextY;
--- ctci1.6/src/test/java/ru/skrser/ctci1dot6/test/SolutionTest.java
+++ ctci1.6/src/test/java/ru/skrser/ctci1dot6/test/SolutionTest.java
@@ -1,8 +1,48 @@
 package ru.skrser.ctci1dot6.test;
 
+import org.junit.Test;
+import ru.skrser.ctci1dot6.Solution;
+
+import static org.junit.Assert.assertArrayEquals;
+
 /**
  * Created by Sergey Skryabin
  * on 01.01.2017.
  */
 public class SolutionTest {
+
+    @Test
+    public void testNull() {
+        Solution.rotate(null);
+    }
+
+    @Test
+    public void test1x1() {
+        byte[][][] image = {{{100, 127, 1, -128}}};
+        final byte[][][] rotatedImage = {{{100, 127, 1, -128}}};
+        Solution.rotate(image);
+        assertArrayEquals(rotatedImage, image);
+    }
+
+    @Test
+    public void test2x2() {
+        byte[][][] image = {{{ 1,  2,  3,  4}, {11, 12, 13, 14}},
+                            {{21, 22, 23, 24}, {31, 32, 33, 34}}};
+        final byte[][][] rotatedImage = {{{21, 22, 23, 24}, { 1,  2,  3,  4}},
+                                        { {31, 32, 33, 34}, {11, 12, 13, 14}}};
+        Solution.rotate(image);
+        assertArrayEquals(rotatedImage, image);
+    }
+
+    @Test
+    public void test3x3() {
+        byte[][][] image = {{{ 1,  2,  3,  4}, {11, 12, 13, 14}, {21, 22, 23, 24}},
+                            {{31, 32, 33, 34}, {41, 42, 43, 44}, {51, 52, 53, 54}},
+                            {{61, 62, 63, 64}, {71, 72, 73, 74}, {81, 82, 83, 84}}};
+        final byte[][][] rotatedImage = {{{61, 62, 63, 64}, {31, 32, 33, 34}, { 1,  2,  3,  4}},
+                                         {{71, 72, 73, 74}, {41, 42, 43, 44}, {11, 12, 13, 14}},
+                                         {{81, 82, 83, 84}, {51, 52, 53, 54}, {21, 22, 23, 24}}};
+        Solution.rotate(image);
+        assertArrayEquals(rotatedImage, image);
+    }
 }
