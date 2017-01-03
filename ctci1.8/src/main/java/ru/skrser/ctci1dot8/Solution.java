b507bd0bd58c7493ebfe8b466b2e84df11f8a9e7
Sergey Skryabin <sskryabin@diasoft.ru>
2017-01-03 12:41:33 +0300

    question 1.8

--- ctci1.8/src/main/java/ru/skrser/ctci1dot8/Solution.java
+++ ctci1.8/src/main/java/ru/skrser/ctci1dot8/Solution.java
@@ -1,4 +1,49 @@
 package ru.skrser.ctci1dot8;
 
 public class Solution {
+
+    /**
+     * Checks if one string is result of rotation of another (e.g. "result" and "sultre"),
+     * using only one execution of given method "isSubstring".
+     * Method is symmetric, i.e. isRotation(s1, s2) == isRotation(s2, s1)
+     * Computational complexity = O(n^2)
+     * Memory consumption = O(1)
+     *
+     * @param s1 first string
+     * @param s2 second string
+     * @return true if s2 is result of rotation of s1
+     */
+    public static boolean isRotation(String s1, String s2) {
+        if (s1 == null)
+            return s2 == null;
+        else if (s2 == null)
+            return false;
+        if (s1.equals(s2)) return true;
+        if (s1.length() != s2.length()) return false;
+
+        int delimiter = findDelimiterPosition(s1, s2);
+        return delimiter != -1 && isSubstring(s1.substring(delimiter, s1.length()), s2.substring(0, s2.length() - delimiter));
+    }
+
+    private static boolean isSubstring(String string, String substring) {
+        return string.contains(substring);
+    }
+
+    private static int findDelimiterPosition(String s1, String s2) {
+        int i = 0;
+        int delta = 0;
+        while (i + delta < s2.length()) {
+            if (s1.charAt(i) == s2.charAt(i + delta))
+                i++;
+            else {
+                delta++;
+                i = 0;
+            }
+        }
+        if (delta != s2.length())
+            return i;
+        else
+            return -1;
+    }
+
 }
--- ctci1.8/src/test/java/ru/skrser/ctci1dot8/test/SolutionTest.java
+++ ctci1.8/src/test/java/ru/skrser/ctci1dot8/test/SolutionTest.java
@@ -1,4 +1,36 @@
 package ru.skrser.ctci1dot8.test;
 
+import org.junit.Test;
+import ru.skrser.ctci1dot8.Solution;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
 public class SolutionTest {
+
+    @Test
+    public void testPositive() {
+        assertTrue(Solution.isRotation(null, null));
+        assertTrue(Solution.isRotation("", ""));
+        assertTrue(Solution.isRotation("a", "a"));
+        assertTrue(Solution.isRotation("ab", "ab"));
+        assertTrue(Solution.isRotation("ab", "ba"));
+        assertTrue(Solution.isRotation("result", "sultre"));
+        assertTrue(Solution.isRotation("waterbattle", "erbattlewat"));
+        assertTrue(Solution.isRotation("waterwa", "wawater"));
+        assertTrue(Solution.isRotation("wawater", "wawater"));
+    }
+
+    @Test
+    public void testNegative() {
+        assertFalse(Solution.isRotation(null, ""));
+        assertFalse(Solution.isRotation("abc", null));
+        assertFalse(Solution.isRotation(null, ""));
+        assertFalse(Solution.isRotation("a", "b"));
+        assertFalse(Solution.isRotation("abc", "ab"));
+        assertFalse(Solution.isRotation("acb", "abc"));
+        assertFalse(Solution.isRotation("reesult", "seultre"));
+        assertFalse(Solution.isRotation("waterwa", "wewater"));
+        assertFalse(Solution.isRotation("waterWa", "wawater"));
+    }
 }
