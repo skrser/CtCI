f03103baacb656056b3080726baa4d13a67411a3
Sergey Skryabin <sskryabin@diasoft.ru>
2017-01-04 14:45:51 +0300

    question 2.2 structure

---
+++ ctci2.2/src/main/java/ru/skrser/ctci2dot2/Node.java
@@ -0,0 +1,8 @@
+package ru.skrser.ctci2dot2;
+
+public class Node {
+
+    public Node next;
+
+    public int data;
+}
---
+++ ctci2.2/src/test/java/ru/skrser/ctci2dot2/test/SolutionTest.java
@@ -0,0 +1,4 @@
+package ru.skrser.ctci2dot2.test;
+
+public class SolutionTest {
+}
